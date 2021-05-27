7408a1568658ddbdc91724b5d8e173253df64e35
Tim Biddington <timbiddington@fastmail.com.au>
2021-05-27 13:31:10 +1000

    Move setup into main.ts away from index.js

--- public/index.js
+++ public/index.js
@@ -1,6 +1,4 @@
-import { randSegment } from '../src/two-d';
-import { drawSegment,drawPoint } from '../src/canvas';
-import { toEvents } from '../src/main';
+import { run } from '../src/main';
 
 let canvas = document.getElementsByTagName('canvas')[0];
 let ctx = canvas.getContext('2d');
@@ -14,26 +12,7 @@ let bounds = {
   yMax: canvas.height
 }
 
-let segments = Array
-                .from({length:10})
-                .map(() => randSegment(bounds));
-
-// Draw segments
-segments
-  .forEach(s => {
-    drawSegment( ctx,s );
-  });
-
-// Draw endpoints of segments
-segments
-  .flatMap(p => p)
-  .sort((a,b) => a - b)
-  .forEach(p => drawPoint(ctx,p))
-
-// Add to queue
-let events = segments
-              .map(toEvents);
-
+run(ctx,bounds);
 //  Queue
 //    - Desc of line segment (y = mx + c)
 //  Sweep Line Status
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { lineDesc, LineDesc, Point, Segment } from "./two-d";
+import { randSegment, Bounds, lineDesc, LineDesc, Point, Segment } from "./two-d";
+import { drawSegment,drawPoint } from './canvas';
 
 type EventType = "start" | "end";
 type Event = { type:EventType, desc:LineDesc, point:Point }
 
-export function sortEvents(a:Event,b:Event){
+export function eventComparer(a:Event,b:Event){
   if ( a.point.x == b.point.x )
     return a.point.y - b.point.y;
   
@@ -26,6 +27,26 @@ export function toEvents(s:Segment): Array<Event> {
 /**
  * 
  */
-export function run(): void {
+export function run(ctx,bounds:Bounds): void {
 
+  let segments = Array
+                .from({length:10})
+                .map(() => randSegment(bounds));
+
+  // Draw segments
+  segments
+    .forEach(s => {
+      drawSegment( ctx,s );
+    });
+
+  // Draw endpoints of segments
+  // segments
+  //   .flatMap(p => p)
+  //   .sort((a,b) => a.x - b.x)
+  //   .forEach(p => drawPoint(ctx,p))
+
+  // Set up our event queue
+  // Project our segments to events
+  let events = segments
+              .flatMap(toEvents);
 }
